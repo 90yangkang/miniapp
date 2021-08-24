755dc18adcd7a3af14de2e27f75847f5177e3e55
yk_ang@outlook.com <yk_ang@outlook.com>
2021-08-24 22:20:37 +0800

    init

---
+++ miniprogram-1/pages/action/index.js
@@ -0,0 +1,15 @@
+Page({
+  data: {},
+  tap1: function (event) {
+    console.log('tap1');
+  },
+  tap2: function (event) {
+    console.log('tap2');
+  },
+  tap3: function (event) {
+    console.log('tap3');
+  },
+  tap4: function (event) {
+    console.log('tap4');
+  },
+})
